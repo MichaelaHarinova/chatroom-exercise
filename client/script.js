05cdf34a9b3ce80630be6ea830e449f52c8c5cae
Micha <michaelaharin@gmail.com>
2021-04-21 14:18:38 +0200

    final version

--- client/script.js
+++ client/script.js
@@ -30,7 +30,7 @@ sock.on('join', (allUsers) => {
     userList.innerHTML = '';
     for (const user of allUsers) {
       //  console.log("test");
-        userList.innerHTML += `<br><ul><li class="list">online <p class="p" id="${user.id}">${user.username}</p></li></ul>`;
+        userList.innerHTML += `<br><ul><li>online <p class="p" id="${user.id}">${user.username}</p></li></ul>`;
         setTimeout(() => {
             document.getElementById(user.id).addEventListener("click", () => {
                 document.getElementById("receiverName").value = user.username;
@@ -60,7 +60,7 @@ target = document.getElementById("target")
 sock.on('displayMessage', (data) => {
     console.log(data);
     document.getElementById('content').value = '';
-    target.innerHTML += '<marquee scrollamount="4" scrolldelay="8" ><p class="p"><span class="username">' + data.user  + '</span> -> ' + data.message + '</marquee><br> ';
+    target.innerHTML += '<marquee scrollamount="4" scrolldelay="8" ><p class="messageGeneral"><span class="username">' + data.user  + '</span> -> ' + data.message + '</marquee><br> ';
     document.getElementById("target").scrollTop = document.getElementById("target").scrollHeight
 });
 
@@ -73,7 +73,7 @@ document.getElementById("send").addEventListener("click", () => {
     let receiverName = document.getElementById("receiverName").value;
 
     document.getElementById('privateMessage').value = '';
-    privateMsg.innerHTML += '<p class="textBreak"><strong>' + username + ' -> ' + receiverName + '</strong>: ' + message + '</p><br>';
+    privateMsg.innerHTML += '<p class="textBreak "><span class="username">' + 'you </span> -> <span class="message">' + receiverName + '</span>: ' + message + '</p><br>';
     sock.emit('private', ({username: username, receiver: receiver,receiverName: receiverName, message: message}));
 });
 
@@ -82,7 +82,7 @@ sock.on("private", (data) => {
     // console.log(data);
     if(sock.id === data.receiver){
         document.getElementById('privateMessage').value = '';
-        privateMsg.innerHTML += '<p class="textBreak"><strong>' + data.username + ' -> </strong>: ' + data.message + '</p>';
+        privateMsg.innerHTML += '<p class="textBreak"><span class="username">' + data.username + '</span> -> <span class="message"> you </span>: ' + data.message + '</p>';
     }
     document.getElementById("privateMsg").scrollTop = document.getElementById("privateMsg").scrollHeight
 });
